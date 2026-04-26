35bf0df9d6e3084a7e9de5d59e7d3c3784acd295
Tenzo <147890912+RealTenzo@users.noreply.github.com>
2026-04-27 05:22:41 +0600

    Update Form1.cs

--- CSHARP SDK TenzoXAuthentication/Form1.cs
+++ CSHARP SDK TenzoXAuthentication/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 using TXAAuth;
 
@@ -7,17 +7,17 @@ namespace CSHARP_SDK_TenzoXAuthentication
     public partial class Form1 : Form
     {
         public static TXA TXA = new TXA(
-          name: "",
-          secret: "",
+          name: "app_name",
+          secret: "TXA-secret",
           version: "1.0"
         );
 
         public Form1()
         {
             InitializeComponent();
-            TXA.Init();
         }
 
+     
 
         private async void button1_Click(object sender, EventArgs e)
         {
